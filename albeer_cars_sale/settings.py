d5d830bda3e185eb3befd3fd29b3082aff1b4ad7
albeeralkhawri <king.alber93@gmail.com>
2020-01-17 22:37:40 +0000

    add cart to settings

--- albeer_cars_sale/settings.py
+++ albeer_cars_sale/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django_forms_bootstrap',
     'accounts',
     'cars',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -67,7 +68,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.media'
+                'django.template.context_processors.media',
+                'cart.contexts.cart_contents'
             ],
         },
     },
