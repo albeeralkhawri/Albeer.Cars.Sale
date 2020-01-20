031416814eb7e0cd91f22e61eeb62e07d63c3ae8
albeeralkhawri <king.alber93@gmail.com>
2020-01-20 01:24:22 +0000

    add a new code to do style of image for Car_details

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -30,3 +30,24 @@ $(function() {
     return false;
     });
 });
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+// for car image in car details
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-opacity-off";
+}
