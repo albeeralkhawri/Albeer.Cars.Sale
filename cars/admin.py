eb5548b1cc1fcf4b2db28f4f4a2f2ce1ae084b46
albeeralkhawri <king.alber93@gmail.com>
2020-01-25 20:07:38 +0000

    add same code for admin file to do more sensitive

--- cars/admin.py
+++ cars/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from .models import Car
 from .models import Car_image
-
+from django import forms
+from django.contrib import admin
+from django.forms import TextInput, Textarea
+from django.db import models
+from .models import Car
 # Register your models here.
-admin.site.register(Car),
 admin.site.register(Car_image)
+
+class CarForm(forms.ModelForm):
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows':1, 'cols':40})},
+    }
+    class Meta:
+        model = Car
+        exclude = []
+class CarAdmin(admin.ModelAdmin):
+    exclude = []
+    form = CarForm
+admin.site.register(Car,CarAdmin)
